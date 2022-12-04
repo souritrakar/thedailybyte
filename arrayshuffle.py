78656494e135e0cb3f6fd35083ae61cc5009475d
Souritra Kar <64901415+souritrakar@users.noreply.github.com>
2022-12-04 20:40:32 +0530

    Update arrayshuffle.py

--- arrayshuffle.py
+++ arrayshuffle.py
@@ -1,7 +1,6 @@
 nums = [1, 2, 3, 4]
 
 for i in range(0, len(nums)-1):
-  print(i,nums[i], nums[i+1])
   if i%2==0 and nums[i]%2!=0 and nums[i+1]%2==0:
     temp = nums[i+1]
     nums[i+1] = nums[i]
