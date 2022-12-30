5f0faeaa8b6764bd905eb7a68c38517f1a24e7de
Souritra Kar <64901415+souritrakar@users.noreply.github.com>
2022-12-30 17:55:31 +0530

    Create wordlength.py

---
+++ wordlength.py
@@ -0,0 +1,18 @@
+s = "The Daily Byte"
+counter = 0
+for i in range(len(s)-1, -1, -1):
+  if s[i]== " ":
+    break
+  else:
+    counter+=1
+
+print(counter)
+
+"""
+Given a string, s, return the length of the last word.
+## Note: You may not use any sort of split() method.
+
+Ex: Given the following string…
+
+s = "The Daily Byte", return 4 (because "Byte" is four characters long).
+"""
